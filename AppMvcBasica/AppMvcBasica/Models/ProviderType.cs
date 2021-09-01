6701d072137493812c95b3106cf3041267e313b5
JoaoLimaBRR <joaovitorsoareslima12@gmail.com>
2021-08-31 22:27:00 -0300

    add controller of providers and add partial view in form create

--- AppMvcBasica/AppMvcBasica/Models/ProviderType.cs
+++ AppMvcBasica/AppMvcBasica/Models/ProviderType.cs
@@ -1,8 +1,12 @@
-﻿namespace AppMvcBasica.Models
+﻿using System.ComponentModel;
+
+namespace AppMvcBasica.Models
 {
     public enum ProviderType
     {
+        [DisplayName("Pessoa Fisíca")]
         PrivateIndividual = 1,
+        [DisplayName("Pessoa Jurídica")]
         LegalPerson = 2
     }
 }
